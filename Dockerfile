94f81a63950ff2d0ca7421cd588cfdf86e9d9059
Simon Walker <simon@simonwalker.tech>
2023-06-22 12:53:27 -0600

    containerizing

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM rust:1.67
-
-WORKDIR /subsetlatticebuilder
-
-COPY data .
-COPY trickle_constr .
-COPY inf_ancestor .
-
-# RUN RUSTFLAGS="-C target-cpu=native" cargo build --release
--- inf_ancestr/src/main.rs
+++ inf_ancestr/src/main.rs
@@ -1,3 +1,8 @@
+use arrayfire as af;
+
 fn main() {
     println!("Hello, world!");
+    println!("ArrayFire Devices: {}", af::device_count());
+    // device details
+    println!("{:?}", af::device_info());
 }
