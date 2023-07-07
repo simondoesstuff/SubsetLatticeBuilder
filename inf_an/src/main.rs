ec65f8b24585597ac20eb67ec9ecc77821633158
Simon Walker <siwa3657@fiji-1.colorado.edu>
2023-07-07 03:19:40 -0600

    task gen & consume in parallel

--- inf_an/src/main.rs
+++ inf_an/src/main.rs
@@ -1,51 +1,11 @@
-// use cudarc::{
-//     driver::{CudaDevice, DriverError, LaunchAsync, LaunchConfig},
-//     nvrtc::Ptx,
-// };
-
-// fn main() {
-//     let dev = CudaDevice::new(0).unwrap();
-// 	println!("{:?}", dev);
-	
-// 	let ptx = cudarc::nvrtc::compile_ptx("
-// 		extern \"C\" __global__ void sin_kernel(float *out, const float *inp, const int numel) {
-// 			unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
-// 			if (i < numel) {
-// 				out[i] = sin(inp[i]);
-// 			}
-// 		}").unwrap();	
-// 	println!("PTX compiled");
-	
-//     dev.load_ptx(ptx, "module", &["sin_kernel"]).unwrap();
-// 	println!("PTX loaded");
-
-//     // and then retrieve the function with `get_func`
-//     let f = dev.get_func("module", "sin_kernel").unwrap();
-
-//     let a_host = [1.0, 2.0, 3.0];
-
-//     let a_dev = dev.htod_copy(a_host.into()).unwrap();
-//     let mut b_dev = a_dev.clone();
-
-//     let n = 3;
-//     let cfg = LaunchConfig::for_num_elems(n);
-//     unsafe { f.launch(cfg, (&mut b_dev, &a_dev, n as i32)) }.unwrap();
-
-//     let a_host_2 = dev.sync_reclaim(a_dev).unwrap();
-//     let b_host = dev.sync_reclaim(b_dev).unwrap();
-
-//     println!("Found {:?}", b_host);
-//     println!("Expected {:?}", a_host.map(f32::sin));
-//     assert_eq!(&a_host, a_host_2.as_slice());
-// }
-
-
 use cudarc::driver::result::device::{get_count as device_count};
-use cudarc::driver::{CudaDevice, LaunchConfig, LaunchAsync};
+use cudarc::driver::{CudaDevice, LaunchConfig, LaunchAsync, CudaSlice};
 use cudarc::driver::result::{init as cuda_driver_init};
 
 use bit_set::BitSet;
 use std::io::BufRead;
+use std::sync::{Arc, mpsc};
+use std::thread;
 
 
 /// Returns the nodes as a bit buffer, the number of nodes, and the length of each node in the buffer
@@ -73,7 +33,8 @@ fn load_data(filename: &str) -> (Vec<bool>, u32, u32) {
         
         nodes.push(set);
     }
-    
+   
+    // todo crunch bits in 64 bit chunks 
     // bit buffer for all nodes 
     let mut node_buffer = Vec::new();
     let node_amnt = nodes.len() as u32;
@@ -93,8 +54,111 @@ fn load_data(filename: &str) -> (Vec<bool>, u32, u32) {
 }
 
 
+fn task_gen_thread(mem_max: usize, node_len: usize, node_amnt: usize) -> mpsc::Receiver<Vec<(u32, u32)>> {
+    let (tx, rx) = mpsc::channel::<Vec<(u32, u32)>>();
+
+    thread::spawn(move || {
+        let max_tasks_per_device = {
+            // max - node_buffer = remaining
+            // (2 * u32) // op1, op2
+            // node_len // output buffer
+            // remaining / (op12 + out) = amnt of tasks
+            (mem_max - node_amnt * node_len)
+            / (2 * 32 + node_len)
+        };
+        
+        let mut generator = (0..node_amnt)
+                        .flat_map(|i| (i+1..node_amnt)
+                        .map(move |j| (i as u32, j as u32)));
+        
+        'outer: loop {
+            let mut next_chunk = Vec::new();
+            
+            while next_chunk.len() < max_tasks_per_device {
+                if let Some(next_pair) = generator.next() {
+                    next_chunk.push(next_pair);
+                } else {
+                    tx.send(next_chunk).unwrap();
+                    break 'outer;
+                }
+            }
+            
+            tx.send(next_chunk).unwrap();
+        }
+    });
+
+    return rx;
+}
+
+
+fn result_consumer_thread(node_len: u32) -> mpsc::Sender<Vec<bool>> {
+    let (tx, rx) = mpsc::channel::<Vec<bool>>();
+
+    thread::spawn(move || {
+        let mut total = 0;
+
+        for result in rx {
+            // we are currently venting results as they come in
+            // let mut results = Vec::new();
+            let mut results = 0;
+
+            for chunk in result.chunks(node_len as usize) {
+                // let mut node = BitSet::new();
+                
+                // for (i, bit) in chunk.iter().enumerate() {
+                //     if *bit {
+                //         node.insert(i + 1);
+                //     }
+                // }
+                
+                // results.push(node);
+                results += 1;
+            }
+            
+            total += results;
+            print!("New Results. {} new of {} total.", results, total);
+            print!("    ");
+            
+            // for result in results {
+            //     print!(" {:?}", result);
+            // }
+            
+            println!();
+        }
+    });
+
+    return tx;
+}
+
+
+fn launch_device(dev: &Arc<CudaDevice>, task: Vec<(u32, u32)>, node_buffer: Vec<bool>, node_len: u32) -> CudaSlice<bool> {
+    let (op1, op2): (Vec<u32>, Vec<u32>) = task.iter().cloned().unzip();
+
+    let n = op1.len();
+    let cfg = LaunchConfig::for_num_elems(n as u32);
+    
+    let dev_op1 = dev.htod_copy(op1).expect("Failed to copy op1 to device");
+    let dev_op2 = dev.htod_copy(op2).expect("Failed to copy op2 to device");
+    let dev_nodes = dev.htod_copy(node_buffer.clone()).expect("Failed to copy nodes to device");
+
+    let mut dev_out = dev.alloc_zeros::<bool>(n * node_len as usize).expect("Failed to allocate output buffer");
+    
+    // get kernel
+    let kernel = dev.get_func("module", "intersect_kernel").expect("Failed to get kernel");
+    
+    unsafe {
+        kernel
+            .launch(cfg, (&mut dev_out, &dev_nodes ,&dev_op1, &dev_op2, node_len as u32, n))
+            .expect("Failed to launch kernel");
+    }
+    
+    return dev_out;
+}
+
+
 fn main() {
-    let input_file = "../data/dirty/3515.txt";
+    let input_file = "../data/dirty/79867.txt";
+    let mem_max = ( (2) * 8*10_usize.pow(9) ) as usize;
     
     
     // intialize CUDA driver
@@ -109,11 +173,16 @@ fn main() {
     
     let devices = {
         let mut devices = Vec::with_capacity(dev_count);
+        
         for i in 0..dev_count {
-            devices.push(CudaDevice::new(i as usize).unwrap_or_else(|e| {
+            let new_device = CudaDevice::new(i as usize).unwrap_or_else(|e| {
                 panic!("Failed to get device {}: {}", i, e);
-            }))
+            });
+
+            // todo check if device has enough memory
+            devices.push(new_device);
         }
+        
         devices
     };
     
@@ -137,117 +206,145 @@ fn main() {
     let (node_buffer, node_amnt, node_len) = load_data(input_file);
     println!("Loaded {} nodes with (max) lengths {}.", node_amnt, node_len);
 
-    // create pairs of nodes to intersect
+    // start the marathon
     
-    println!("Creating and dicing tasks...");
-    
-    let mut pairs = Vec::new();
-    
-    for i in 0..node_amnt {
-        for j in i+1..node_amnt {
-            let i = i as u32;
-            let j = j as u32;
-            pairs.push((i, j));
-        }
-    }
-    
-    // break the pairs into chunks for each device
-    let chunk_size = pairs.len() / dev_count;
-    let mut pairs_chunked = Vec::new();
-    
-    for i in 0..dev_count {
-        let start = i * chunk_size;
-        let end = if i == dev_count- 1 { pairs.len() } else { (i + 1) * chunk_size };
-        pairs_chunked.push(&pairs[start..end]);
-    }
+    println!("Creating and dicing tasks in the background.");
+    // Thread:    Generates tasks
+    let task_rx = task_gen_thread(mem_max, node_len as usize, node_amnt as usize);
+    // Thread:    Handles results
+    let result_tx = result_consumer_thread(node_len);
     
     // initialize buffers and launch kernels
     
-    println!("\nLaunching kernels...");
+    println!("Launching kernels...\n");
+    
+    // repeatedly send and receive GPU workers
+    let mut has_tasks = true;
+    while has_tasks {
+        let mut out_buffers = Vec::new();
 
-    let mut dev_out_buffers = Vec::new();
-    
-    // allocate buffers on each device
-    for (i, dev) in devices.iter().enumerate() {
-        // pairs is two separate buffers on the device
-        let (op1, op2): (Vec<u32>, Vec<u32>) = pairs_chunked[i].iter().cloned().unzip();
-
-        let n = op1.len();
-        let cfg = LaunchConfig::for_num_elems(n as u32);
-        
-        let dev_op1 = dev.htod_copy(op1).expect("Failed to copy op1 to device");
-        let dev_op2 = dev.htod_copy(op2).expect("Failed to copy op2 to device");
-        let dev_nodes = dev.htod_copy(node_buffer.clone()).expect("Failed to copy nodes to device");
-
-        // todo out buffer should be a bit buffer
-        let dev_out = dev.alloc_zeros::<bool>(n * node_len as usize).expect("Failed to allocate output buffer");
-        dev_out_buffers.push(dev_out);
-        
-        // get kernel
-        let kernel = dev.get_func("module", "intersect_kernel").expect("Failed to get kernel");
-        
-        unsafe {
-            kernel
-                .launch(cfg, (&mut dev_out_buffers[i], &dev_nodes ,&dev_op1, &dev_op2, node_len as u32, n))
-                .expect("Failed to launch kernel");
-        }
-        
-        println!("Launched device {}", i);
-    }
-    
-    // reclaim buffers from each device
-    
-    println!();
-    
-    let mut raw_results = Vec::new();
-    
-    for (i, dev) in devices.iter().enumerate().rev() {
-        let dev_out = dev_out_buffers.pop().unwrap();
-        let out = dev.sync_reclaim(dev_out).expect("Failed to reclaim buffer");
-        raw_results.push(out); // results is in reverse order
-    
-        println!("Reclaimed device {}", i);
-    }
-    
-    // aggregate results
-
-    println!("\nAggregating results...");
-    
-    let mut results_aggregated = Vec::new();
-    
-    for (_, bit_chunk) in raw_results.iter().enumerate().rev() {
-        let mut results = Vec::new();
-        
-        for chunk in bit_chunk.chunks(node_len as usize) {
-            let mut node = BitSet::new();
+        for dev in &devices {
+            let task = match task_rx.recv() {
+                Ok(task) => task,
+                Err(_) => {
+                    has_tasks = false;
+                    break;
+                },
+            };
             
-            for (i, bit) in chunk.iter().enumerate() {
-                if *bit {
-                    node.insert(i + 1);
-                }
-            }
-            
-            results.push(node);
+            let out_buffer = launch_device(dev, task, node_buffer.clone(), node_len);
+            out_buffers.push(out_buffer);
+            println!("-->   Launched device {}", dev.ordinal());
         }
         
-        results_aggregated.push(results);
-    }
-    
-    // print results
-    
-    println!("Results:");
-    
-    let total_results = results_aggregated.iter().fold(0, |acc, x| acc + x.len());
-    
-    for (i, results) in results_aggregated.iter().enumerate() {
-        print!("Dev {}:", i);
-        
-        for result in results {
-            print!(" {:?}", result);
+        // enumerate through out_buffers in reverse order, without consuming it by popping buffers off the end
+        for i in (0..out_buffers.len()).rev() {
+            let dev = &devices[i];
+            let out_buffer = out_buffers.pop().unwrap();
+            let result = dev.sync_reclaim(out_buffer).expect("Failed to reclaim buffer");
+            result_tx.send(result).unwrap();
+            println!("<--   Reclaimed device {}", dev.ordinal());
         }
-        
-        println!();
-    }
-
-    println!("\nAccumulated {} results.", total_results);
+    } // sender should automatically be dropped when tasks are done
+    
+    println!("All tasks complete.");
 }
+    
+
+
+
+
+
+
+
+
+
+
+
+    // let mut dev_out_buffers = Vec::new();
+    
+    // // allocate buffers on each device
+    // for (i, dev) in devices.iter().enumerate() {
+    //     // pairs is two separate buffers on the device
+    //     let (op1, op2): (Vec<u32>, Vec<u32>) = pairs_chunked[i].iter().cloned().unzip();
+
+    //     let n = op1.len();
+    //     let cfg = LaunchConfig::for_num_elems(n as u32);
+        
+    //     let dev_op1 = dev.htod_copy(op1).expect("Failed to copy op1 to device");
+    //     let dev_op2 = dev.htod_copy(op2).expect("Failed to copy op2 to device");
+    //     let dev_nodes = dev.htod_copy(node_buffer.clone()).expect("Failed to copy nodes to device");
+
+    //     // todo out buffer should be a bit buffer
+    //     let dev_out = dev.alloc_zeros::<bool>(n * node_len as usize).expect("Failed to allocate output buffer");
+    //     dev_out_buffers.push(dev_out);
+        
+    //     // get kernel
+    //     let kernel = dev.get_func("module", "intersect_kernel").expect("Failed to get kernel");
+        
+    //     unsafe {
+    //         kernel
+    //             .launch(cfg, (&mut dev_out_buffers[i], &dev_nodes ,&dev_op1, &dev_op2, node_len as u32, n))
+    //             .expect("Failed to launch kernel");
+    //     }
+        
+    //     println!("Launched device {}", i);
+    // }
+    
+    // // reclaim buffers from each device
+    
+    // println!();
+    
+    // let mut raw_results = Vec::new();
+    
+    // for (i, dev) in devices.iter().enumerate().rev() {
+    //     let dev_out = dev_out_buffers.pop().unwrap();
+    //     let out = dev.sync_reclaim(dev_out).expect("Failed to reclaim buffer");
+    //     raw_results.push(out); // results is in reverse order
+    
+    //     println!("Reclaimed device {}", i);
+    // }
+    
+    // // aggregate results
+    // // todo wayyy too slow
+
+    // println!("\nAggregating results...");
+    
+    // let mut results_aggregated = Vec::new();
+    
+    // for (_, bit_chunk) in raw_results.iter().enumerate().rev() {
+    //     let mut results = Vec::new();
+        
+    //     for chunk in bit_chunk.chunks(node_len as usize) {
+    //         let mut node = BitSet::new();
+            
+    //         for (i, bit) in chunk.iter().enumerate() {
+    //             if *bit {
+    //                 node.insert(i + 1);
+    //             }
+    //         }
+            
+    //         results.push(node);
+    //     }
+        
+    //     results_aggregated.push(results);
+    // }
+    
+    // // print results
+    
+    // println!("Results:");
+    
+    // let total_results = results_aggregated.iter().fold(0, |acc, x| acc + x.len());
+    
+    // for (i, results) in results_aggregated.iter().enumerate() {
+    //     print!("Dev {}:", i);
+        
+    //     for result in results {
+    //         print!(" {:?}", result);
+    //     }
+        
+    //     println!();
+    // }
+
+    // println!("\nAccumulated {} results.", total_results);
+// }
