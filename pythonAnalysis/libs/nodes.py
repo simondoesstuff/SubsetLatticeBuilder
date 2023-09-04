89f36fdb68769149e741cb7d910aaf5b35590e27
Simon Walker <simon@simonwalker.tech>
2023-09-04 00:49:50 -0600

    adj intersect data

--- pythonAnalysis/libs/nodes.py
+++ pythonAnalysis/libs/nodes.py
@@ -1,35 +1,40 @@
-from typing import AbstractSet, Iterable
+from typing import AbstractSet, Iterable, Self
 
 
 class node(frozenset):
+    def __init__(self, initial: str | Iterable = None) -> None:
+        if isinstance(initial, str):
+            initial = [int(x) for x in initial.split()]
+        super().__new__(node, initial)
+
     def __repr__(self) -> str:
         return f'({",".join(str(x) for x in sorted(self))})'
     
     def copy(self):
         return self
     
-    def difference(self, other: Iterable) -> 'node':
+    def difference(self, other: Iterable) -> Self:
         return node(super().difference(other))
     
-    def intersection(self, other: Iterable) -> 'node':
+    def intersection(self, other: Iterable) -> Self:
         return node(super().intersection(other))
     
-    def union(self, other: Iterable) -> 'node':
+    def union(self, other: Iterable) -> Self:
         return node(super().union(other))
     
-    def symmetric_difference(self, other: Iterable) -> 'node':
+    def symmetric_difference(self, other: Iterable) -> Self:
         return node(super().symmetric_difference(other))
     
-    def __and__(self, other: AbstractSet) -> 'node':
+    def __and__(self, other: AbstractSet) -> Self:
         return self.intersection(other)
     
-    def __or__(self, other: AbstractSet) -> 'node':
+    def __or__(self, other: AbstractSet) -> Self:
         return self.union(other)
     
-    def __xor__(self, other: AbstractSet) -> 'node':
+    def __xor__(self, other: AbstractSet) -> Self:
         return self.symmetric_difference(other)
     
-    def __sub__(self, other: AbstractSet) -> 'node':
+    def __sub__(self, other: AbstractSet) -> Self:
         return self.difference(other)
     
     def __ge__(self, other) -> bool:
@@ -51,6 +56,9 @@ class node(frozenset):
         if isinstance(other, int):
             return len(self) < other
         return super().__lt__(other)
+    
+    def __bool__(self) -> bool:
+        return len(self) > 0
 
 
 def parse_nodes(path: str) -> list[node]:
@@ -62,7 +70,8 @@ def parse_nodes(path: str) -> list[node]:
 
 def write_nodes(path: str, nodes: list[node]) -> None:
     with open(path, 'w') as f:
-        for result in nodes:
-            f.write(' '.join([str(x) for x in result]))
+        for node in nodes:
+            node = sorted(node)
+            f.write(' '.join([str(x) for x in node]))
             f.write('\n')
 
